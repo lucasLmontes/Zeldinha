5a67239be0976f73dbb240bb4debab621f636bdb
Lucas <Lucas@DESKTOP-U5QBEMH.local>
2025-01-09 15:17:42 -0300

    Criando Classes: Spritesheet; Entity; Player.

--- zeldinha/src/com/zeldinha/main/Game.java
+++ zeldinha/src/com/zeldinha/main/Game.java
@@ -1,4 +1,4 @@
-package zeldinha;
+package com.zeldinha.main;
 
 import java.awt.Canvas;
 import java.awt.Color;
@@ -6,8 +6,14 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
+
+import com.zeldinha.entities.Entity;
+import com.zeldinha.entities.Player;
+import com.zeldinha.graficos.Spritesheet;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
@@ -17,10 +23,16 @@ public class Game extends Canvas implements Runnable {
 	private boolean isRunning;
 	private BufferedImage image;
 	
+	public List<Entity> entities;
+	public Spritesheet spritesheet;
+	
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 		initFrame();
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		entities = new ArrayList<Entity>();
+		spritesheet = new Spritesheet("/spritesheet.png");
+		entities.add(new Player(0, 0, 16, 16, spritesheet.getSprite(32, 0, 16, 16)));
 	}
 	
 	public void initFrame() {
@@ -54,7 +66,10 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void tick() {
-		
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.tick();
+		}
 	}
 	
 	public void render() {
@@ -67,6 +82,11 @@ public class Game extends Canvas implements Runnable {
 		Graphics g = image.getGraphics();
 		g.setColor(new Color(0, 0, 0));
 		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
 		
 		g.dispose();
 		g = bs.getDrawGraphics();
